b1af32fe8cd0ec1fcc92ed077a5e298250a0c941
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-10 19:37:00 +0900

    Feat: 기본 응답 형식 설계, 공통 예외 처리

---
+++ src/main/java/com/umc/site/global/apiPayload/exception/GeneralException.java
@@ -0,0 +1,18 @@
+package com.umc.site.global.apiPayload.exception;
+
+import com.umc.site.global.apiPayload.code.BaseErrorCode;
+import com.umc.site.global.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+    private BaseErrorCode code;
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
