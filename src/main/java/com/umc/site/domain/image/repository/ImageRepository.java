29011aca3bae90665ae3e05bcfb1f3af3f0d6b35
dokyung-kang <kangdk02@naver.com>
2025-02-11 14:15:02 +0900

    Feat: 리포지토리 메서드 추가

--- src/main/java/com/umc/site/domain/image/repository/ImageRepository.java
+++ src/main/java/com/umc/site/domain/image/repository/ImageRepository.java
@@ -2,10 +2,13 @@ package com.umc.site.domain.image.repository;
 
 import com.umc.site.domain.club_admin.entity.ClubAdmin;
 import com.umc.site.domain.image.entity.Image;
+import com.umc.site.domain.project.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
     Image findByClubAdmin(ClubAdmin clubAdmin);
+
+    Image findByProject(Project project);
 
 }
