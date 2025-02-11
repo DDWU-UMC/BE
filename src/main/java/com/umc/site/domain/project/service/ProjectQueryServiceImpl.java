f0398e2dc072c05b18c8687052c0f6d3ae7be212
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-11 23:55:34 +0900

    Feat: 프로젝트 상세 정보 조회 Service 작성

--- src/main/java/com/umc/site/domain/project/service/ProjectQueryServiceImpl.java
+++ src/main/java/com/umc/site/domain/project/service/ProjectQueryServiceImpl.java
@@ -1,11 +1,15 @@
 package com.umc.site.domain.project.service;
 
+import com.umc.site.domain.feature.entity.Feature;
+import com.umc.site.domain.feature.repository.FeatureRepository;
 import com.umc.site.domain.image.entity.Image;
 import com.umc.site.domain.image.repository.ImageRepository;
 import com.umc.site.domain.project.converter.ProjectConverter;
 import com.umc.site.domain.project.dto.ProjectResponseDTO;
 import com.umc.site.domain.project.entity.Project;
 import com.umc.site.domain.project.repository.ProjectRepository;
+import com.umc.site.global.apiPayload.code.status.ErrorStatus;
+import com.umc.site.global.apiPayload.exception.handler.ProjectHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,7 @@ import java.util.stream.Collectors;
 public class ProjectQueryServiceImpl implements ProjectQueryService{
     private final ProjectRepository projectRepository;
     private final ImageRepository imageRepository;
+    private final FeatureRepository featureRepository;
 
     // 기수별 프로젝트 목록 조회
     @Override
@@ -32,5 +37,15 @@ public class ProjectQueryServiceImpl implements ProjectQueryService{
                     return ProjectConverter.toProjectPreviewDTO(project, image);
                 })
                 .collect(Collectors.toList());
+    }
+
+    // 프로젝트 상세 정보 조회
+    @Override
+    public ProjectResponseDTO.ProjectDetailDTO getProjectDetails(Long projectId) {
+        Project project = projectRepository.findById(projectId).orElseThrow(() -> new ProjectHandler(ErrorStatus.PROJECT_NOT_FOUND));
+        Image image = imageRepository.findByProject(project);
+        List<Feature> features = featureRepository.findAllByProject(project);
+
+        return ProjectConverter.toProjectDetailDTO(project, image, features);
     }
 }
