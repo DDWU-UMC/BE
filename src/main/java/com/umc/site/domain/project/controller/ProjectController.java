178d52c37ea2c9fadfdba7b13e9c2eb56b72ab3b
dokyung-kang <kangdk02@naver.com>
2025-02-11 15:44:03 +0900

    Refactor: api경로 path -> query 파라미터 변경

--- src/main/java/com/umc/site/domain/project/controller/ProjectController.java
+++ src/main/java/com/umc/site/domain/project/controller/ProjectController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,11 +19,11 @@ public class ProjectController {
 
     // 기수별 프로젝트 목록 조회
     @Operation(summary = "기수별 프로젝트 목록 조회", description = " 기수별 프로젝트 리스트를 불러옵니다.")
-    @GetMapping("/projects/{cohortId}")
+    @GetMapping("/projects")
     @Parameters({
-            @Parameter(name = "cohortId", description = "기수의 ID, path variable 입니다.")
+            @Parameter(name = "cohortId", description = "기수의 ID, query variable 입니다.")
     })
-    public ApiResponse<List<ProjectResponseDTO.ProjectPreviewDTO>> getProjectPreviews(@PathVariable(name = "cohortId") Long cohortId) {
+    public ApiResponse<List<ProjectResponseDTO.ProjectPreviewDTO>> getProjectPreviews(@RequestParam(name = "cohortId") Long cohortId) {
 
         return ApiResponse.onSuccess(projectQueryService.getProjectPreviewList(cohortId));
     }
