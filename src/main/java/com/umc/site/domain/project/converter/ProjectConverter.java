865490a7271387c7a73ccf41da09ee35d6ab9d42
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-11 23:52:05 +0900

    Feat: dto, converter 작성

--- src/main/java/com/umc/site/domain/project/converter/ProjectConverter.java
+++ src/main/java/com/umc/site/domain/project/converter/ProjectConverter.java
@@ -1,9 +1,13 @@
 package com.umc.site.domain.project.converter;
 
+import com.umc.site.domain.feature.converter.FeatureConverter;
+import com.umc.site.domain.feature.entity.Feature;
 import com.umc.site.domain.image.converter.ImageConverter;
 import com.umc.site.domain.image.entity.Image;
 import com.umc.site.domain.project.dto.ProjectResponseDTO;
 import com.umc.site.domain.project.entity.Project;
+
+import java.util.List;
 
 public class ProjectConverter {
 
@@ -16,7 +20,28 @@ public class ProjectConverter {
                 .pm(project.getPm())
                 .frontEnd(project.getFrontEnd())
                 .backEnd(project.getBackEnd())
+                .design(project.getDesign())
                 .serviceType(project.getServiceType())
+                .image(ImageConverter.toImageDTO(image))
+                .build();
+    }
+
+    // 프로젝트 상세 정보 조회
+    public static ProjectResponseDTO.ProjectDetailDTO toProjectDetailDTO(Project project, Image image, List<Feature> features) {
+
+        return ProjectResponseDTO.ProjectDetailDTO.builder()
+                .projectId(project.getId())
+                .title(project.getTitle())
+                .serviceType(project.getServiceType())
+                .pm(project.getPm())
+                .frontEnd(project.getFrontEnd())
+                .backEnd(project.getBackEnd())
+                .design(project.getDesign())
+                .description(project.getDescription())
+                .cohort(project.getCohort().getName())
+                .features(features.stream()
+                        .map(FeatureConverter::toFeatureDTO)
+                        .toList())
                 .image(ImageConverter.toImageDTO(image))
                 .build();
     }
