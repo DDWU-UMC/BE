025d3acc868d09ba659e802995c9f72e9298f200
dokyung-kang <kangdk02@naver.com>
2025-02-14 02:37:04 +0900

    Feat: 프로젝트 생성 Controller 추가

--- src/main/java/com/umc/site/domain/project/controller/ProjectController.java
+++ src/main/java/com/umc/site/domain/project/controller/ProjectController.java
@@ -1,13 +1,18 @@
 package com.umc.site.domain.project.controller;
 
+import com.umc.site.domain.project.dto.ProjectRequestDTO;
 import com.umc.site.domain.project.dto.ProjectResponseDTO;
+import com.umc.site.domain.project.service.ProjectCommandService;
 import com.umc.site.domain.project.service.ProjectQueryService;
 import com.umc.site.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -16,6 +21,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ProjectController {
     private final ProjectQueryService projectQueryService;
+    private final ProjectCommandService projectCommandService;
 
     // 기수별 프로젝트 목록 조회
     @Operation(summary = "기수별 프로젝트 목록 조회", description = " 기수별 프로젝트 리스트를 불러옵니다.")
@@ -48,5 +54,14 @@ public class ProjectController {
     public ApiResponse<List<ProjectResponseDTO.OtherProjectDTO>> getOtherProjects(@PathVariable(name = "projectId") Long projectId) {
 
         return ApiResponse.onSuccess(projectQueryService.getOtherProjects(projectId));
+    }
+    
+    // 프로젝트 생성
+    @Operation(summary = "프로젝트 생성", description = "프로젝트를 생성합니다.")
+    @PostMapping(value =  "/projects", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ApiResponse<ProjectResponseDTO.CreateProjectResultDTO> createProject(@RequestPart("request") @Valid ProjectRequestDTO.CreateProjectDTO request,
+                                                                                @RequestPart("file") MultipartFile file) {
+
+        return ApiResponse.onSuccess(projectCommandService.createProject(request, file));
     }
 }
