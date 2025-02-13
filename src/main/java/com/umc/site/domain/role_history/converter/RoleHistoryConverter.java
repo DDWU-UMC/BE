ef1f81a71eda87be92a0a356fa917336c3926aa1
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-14 05:06:51 +0900

    Feat: 활동 이력 생성 관련 converter 작성

--- src/main/java/com/umc/site/domain/role_history/converter/RoleHistoryConverter.java
+++ src/main/java/com/umc/site/domain/role_history/converter/RoleHistoryConverter.java
@@ -1,10 +1,14 @@
 package com.umc.site.domain.role_history.converter;
 
+import com.umc.site.domain.club_admin.entity.ClubAdmin;
+import com.umc.site.domain.club_admin.entity.enums.Role;
+import com.umc.site.domain.role_history.dto.RoleHistoryRequestDTO;
 import com.umc.site.domain.role_history.dto.RoleHistoryResponseDTO;
 import com.umc.site.domain.role_history.entity.RoleHistory;
 
 public class RoleHistoryConverter {
 
+    // 운영진 리스트 조회
     public static RoleHistoryResponseDTO.RoleHistoryDTO toRoleHistoryDTO(RoleHistory roleHistory) {
 
         return RoleHistoryResponseDTO.RoleHistoryDTO.builder()
@@ -13,5 +17,11 @@ public class RoleHistoryConverter {
                 .build();
     }
 
-
+    // 활동 이력 생성
+    public static RoleHistory toRoleHistory(RoleHistoryRequestDTO.CreateRoleHistoryDTO request, ClubAdmin clubAdmin) {
+        return RoleHistory.builder()
+                .content(request.getContent())
+                .clubAdmin(clubAdmin)
+                .build();
+    }
 }
