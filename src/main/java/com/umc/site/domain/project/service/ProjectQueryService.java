90d4537e3c91f3e64bb106369ff01c8c0e9d48c4
ParkSenn <102174849+ParkSenn@users.noreply.github.com>
2025-02-14 02:52:57 +0900

    Feat: service 메서드 수정

--- src/main/java/com/umc/site/domain/project/service/ProjectQueryService.java
+++ src/main/java/com/umc/site/domain/project/service/ProjectQueryService.java
@@ -1,13 +1,14 @@
 package com.umc.site.domain.project.service;
 
 import com.umc.site.domain.project.dto.ProjectResponseDTO;
+import com.umc.site.domain.project.enums.ServiceType;
 
 import java.util.List;
 
 public interface ProjectQueryService {
 
     // 기수별 프로젝트 목록 조회
-    List<ProjectResponseDTO.ProjectPreviewDTO> getProjectPreviewList(Long cohortId);
+    List<ProjectResponseDTO.ProjectPreviewDTO> getProjectPreviewList(Long cohortId, ServiceType type, String keyword);
 
     // 프로젝트 상세 정보 조회
     ProjectResponseDTO.ProjectDetailDTO getProjectDetails(Long projectId);
