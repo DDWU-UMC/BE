ea1a449bf2c2c482bbdbdb940c022895abb63f93
dokyung-kang <kangdk02@naver.com>
2025-02-14 11:53:35 +0900

    Feat: 기수 수정 관련 Service 추가

--- src/main/java/com/umc/site/domain/cohort/service/CohortCommandServiceImpl.java
+++ src/main/java/com/umc/site/domain/cohort/service/CohortCommandServiceImpl.java
@@ -5,6 +5,8 @@ import com.umc.site.domain.cohort.dto.CohortRequestDTO;
 import com.umc.site.domain.cohort.dto.CohortResponseDTO;
 import com.umc.site.domain.cohort.entity.Cohort;
 import com.umc.site.domain.cohort.repository.CohortRepository;
+import com.umc.site.global.apiPayload.code.status.ErrorStatus;
+import com.umc.site.global.apiPayload.exception.handler.CohortHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,5 +26,20 @@ public class CohortCommandServiceImpl implements CohortCommandService {
         cohort = cohortRepository.save(cohort);
 
         return CohortConverter.toCreateCohortResultDTO(cohort);
+    }
+
+
+    // 기수 수정
+    @Override
+    @Transactional
+    public CohortResponseDTO.UpdateCohortResultDTO updateCohort(Long cohortId, CohortRequestDTO.UpdateCohortDTO request){
+        Cohort cohort = cohortRepository.findById(cohortId)
+                .orElseThrow(() -> new CohortHandler(ErrorStatus.COHORT_NOT_FOUND));
+
+        cohort.setName(request.getName());
+
+        cohort = cohortRepository.save(cohort);
+
+        return CohortConverter.toUpdateCohortResultDTO(cohort);
     }
 }
