199c0777beb555eafbc966040f6d624c25d89357
dokyung-kang <kangdk02@naver.com>
2025-02-18 00:42:29 +0900

    Feat: ENUM 타입 추가

--- src/main/java/com/umc/site/domain/project/enums/ServiceType.java
+++ src/main/java/com/umc/site/domain/project/enums/ServiceType.java
@@ -1,8 +1,30 @@
 package com.umc.site.domain.project.enums;
 
+import lombok.Getter;
+
+
+@Getter
 public enum ServiceType {
-    ALL,
-    WEB,
-    ANDROID,
-    IOS
+    ALL("ALL"),
+    WEB("WEB"),
+    ANDROID("ANDROID"),
+    IOS("IOS"),
+    WEB_ANDROID("WEB/ANDROID"),
+    WEB_IOS("WEB/IOS");
+
+    private final String value;
+
+    ServiceType(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        if (this == WEB_ANDROID) {
+            return "WEB/ANDROID";
+        } else if (this == WEB_IOS) {
+            return "WEB/IOS";
+        }
+        return value;
+    }
 }
